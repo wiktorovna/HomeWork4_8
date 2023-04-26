54619f43dcf30197c777ca2133a07f3a00d4db1f
wiktorovna <wiktorovna@mail.ru>
2023-04-26 22:37:43 +0300

    lesson 6

---
+++ lesson6/task6_4.py
@@ -0,0 +1,44 @@
+# -*- coding: cp1251 -*-
+class Cars:
+    name = None
+    speed = None
+    color = None
+    is_police = False
+
+    def __init__(self, name, speed, color, is_police = False):
+        self.name = name
+        self.speed = speed
+        self.color = color
+        self.is_police = is_police
+    def go(self):
+        return 'Машина тронулась'
+    def stop(self):
+        return 'Машина остановилась'
+    def turn(self, direction):
+        return 'Машина повернула' + direction
+
+class TownCar(Cars):
+    family = None
+    def __init__(self, name, speed, color, family = True):
+        super().__init__(name, speed, color)
+        self.family = family
+
+class SportCar(Cars):
+    def __init__(self, name, speed, color):
+        super().__init__(name, speed, color)
+
+class WorkCar(Cars):
+    def __init__(self, name, speed, color, is_police):
+        super().__init__(name, speed, color, is_police)
+
+class PoliceCar(Cars):
+    def __init__(self, name, speed, color):
+        super().__init__(name, speed, color, True)
+
+volkswagen = TownCar('Фольксваген', 60, 'белый')
+print(volkswagen.name, volkswagen.color, volkswagen.speed, volkswagen.is_police)
+print(volkswagen.go(), volkswagen.turn('налево'), volkswagen.stop())
+sport = SportCar('Фольксваген', 90, 'зеленый')
+work1 = WorkCar('Фольксваген', 70, 'белый', True)
+work2 = WorkCar('бмв', 90, 'красный', False)
+police = PoliceCar('Фольксваген', 180, 'красный')
